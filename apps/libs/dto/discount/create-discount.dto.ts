09c8c53760ffcfffbdd9714764cd7e9e4b7da1db
fchandiac <felipe.chandia.cast@gmail.com>
2024-09-28 06:20:12 -0300

    Create Libs/Enums

--- apps/libs/dto/discount/create-discount.dto.ts
+++ apps/libs/dto/discount/create-discount.dto.ts
@@ -1,37 +1,10 @@
 import { IsEnum } from 'class-validator';
 
-export enum DiscountStatus {
-  CREATED = 0, // Cupón recién creado y disponible
-  TAKEN = 1, // Cupón tomado por un usuario pero no usado
-  REDEEMED = 2, // Cupón canjeado o utilizado
-  EXPIRED = 3, // Cupón que ha expirado antes de su uso
-}
+import { Gender, DiscountStatus, DiscountType, Neighborhoods } from 'apps/libs/enums';
 
-export enum DiscountType {
-  SERVICE = 0, // Cupón para un servicio
-  PRODUCT = 1, // Cupón para un producto
-  COMMERCE = 2, // Cupón para un comercio
-}
 
-export enum DiscountGender {
-  ALL = 0, // Todos los géneros
-  MALE = 1, // Solo hombres
-  FEMALE = 2, // Solo mujeres
-  OTHER = 3, // Otros géneros
-}
 
-export enum DiscountNeighborhood {
-  ALL = 0, // Todos los barrios
-  CENTRO = 1, // Solo barrios locales
-  BUENOS_AIRES = 2, // Solo barrios no locales
-  ARRAU_MENDEZ = 3, // Solo barrios no locales
-  IGUALDAD_SUR = 4, // Solo barrios no locales
-  LOS_OLIVOS = 5, // Solo barrios no locales
-  VIÑA_DEL_MAR = 6, // Solo barrios no locales
-  CAMINO_A_CATILLO = 7, // Solo barrios no locales
-  CAMINO_A_BULLILEO = 8, // Solo barrios no locales
-  TALQUITA = 9, // Solo barrios no locales
-}
+
 
 export class CreateDiscountDto {
   name!: string;
@@ -52,9 +25,9 @@ export class CreateDiscountDto {
   minAge?: number | null;
   maxAge?: number | null;
 
-  @IsEnum(DiscountNeighborhood)
+  @IsEnum(Neighborhoods)
   neighborhood?: number | null;
 
-  @IsEnum(DiscountGender)
+  @IsEnum(Gender)
   gender?: number | null;
 }
--- apps/libs/dto/profile/create-profile.dto.ts
+++ apps/libs/dto/profile/create-profile.dto.ts
@@ -1,24 +1,6 @@
 import { IsNotEmpty, IsDateString, IsUUID, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Gender, Neighborhoods } from 'apps/libs/enums';
 
-export enum DiscountGender {
-  ALL = 0, // Todos los géneros
-  MALE = 1, // Solo hombres
-  FEMALE = 2, // Solo mujeres
-  OTHER = 3, // Otros géneros
-}
-
-export enum DiscountNeighborhood {
-  ALL = 0, // Todos los barrios
-  CENTRO = 1, // Solo barrios locales
-  BUENOS_AIRES = 2, // Solo barrios no locales
-  ARRAU_MENDEZ = 3, // Solo barrios no locales
-  IGUALDAD_SUR = 4, // Solo barrios no locales
-  LOS_OLIVOS = 5, // Solo barrios no locales
-  VIÑA_DEL_MAR = 6, // Solo barrios no locales
-  CAMINO_A_CATILLO = 7, // Solo barrios no locales
-  CAMINO_A_BULLILEO = 8, // Solo barrios no locales
-  TALQUITA = 9, // Solo barrios no locales
-}
 
 export class CreateProfileDto {
   @IsDateString()
@@ -30,11 +12,11 @@ export class CreateProfileDto {
   userId: string;
 
   @IsOptional()
-  @IsEnum(DiscountGender)
+  @IsEnum(Gender)
   gender?: number | null;
 
   @IsOptional()
-  @IsEnum(DiscountNeighborhood)
+  @IsEnum(Neighborhoods)
   neighborhood?: number | null;
 
   
